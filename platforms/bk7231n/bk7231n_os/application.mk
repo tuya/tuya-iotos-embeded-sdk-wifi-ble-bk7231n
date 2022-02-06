61b4204aa0677a8fc886f4e6bd4e235337eb1e85
openshwprojects <openshwprojects@gmail.com>
2022-02-06 18:13:51 +0100

    compile fix

--- platforms/bk7231n/bk7231n_os/application.mk
+++ platforms/bk7231n/bk7231n_os/application.mk
@@ -2,7 +2,12 @@ TOP_DIR = ../../..
 
 # Initialize tool chain  /usr/bin/gcc-arm-none-eabi-4_9-2015q1/bin
 # -------------------------------------------------------------------
-ARM_GCC_TOOLCHAIN = ../toolchain/gcc-arm-none-eabi-4_9-2015q1/bin
+ifeq ($(shell uname), Linux)
+  TOOLCHAIN_DIR := ../toolchain
+else
+  TOOLCHAIN_DIR := ../toolchain/windows
+endif
+ARM_GCC_TOOLCHAIN = $(TOOLCHAIN_DIR)/gcc-arm-none-eabi-4_9-2015q1/bin
 
 CROSS_COMPILE = $(ARM_GCC_TOOLCHAIN)/arm-none-eabi-
 
