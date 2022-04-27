e1afc57b0322d28488b00ca981b4bc509bc00445
openshwprojects <85486843+openshwprojects@users.noreply.github.com>
2022-04-27 12:43:36 +0200

    Update application.mk
    #define PLATFORM_BEKEN

--- platforms/bk7231n/bk7231n_os/application.mk
+++ platforms/bk7231n/bk7231n_os/application.mk
@@ -905,6 +905,7 @@ CFLAGS =
 CFLAGS += -g -mthumb -mcpu=arm968e-s -march=armv5te -mthumb-interwork -mlittle-endian -Os -std=c99 -ffunction-sections -Wall -fsigned-char -fdata-sections -Wunknown-pragmas -nostdlib -Wno-unused-function -Wno-unused-but-set-variable
 
 CFLAGS += -DPLATFORM_BK7231N=1
+CFLAGS += -DPLATFORM_BEKEN=1
 
 OSFLAGS =
 OSFLAGS += -g -marm -mcpu=arm968e-s -march=armv5te -mthumb-interwork -mlittle-endian -Os -std=c99 -ffunction-sections -Wall -fsigned-char -fdata-sections -Wunknown-pragmas
