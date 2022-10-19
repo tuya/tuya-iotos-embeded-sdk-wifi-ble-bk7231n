37c82f2f7aa6f4393951bf7b558c4d0b4c928f63
valeklubomir <valeklubomir@gmail.com>
2022-10-19 14:51:59 +0200

    Linker script fix for CPP Support

--- b_full.sh
+++ b_full.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./build_app.sh apps/OpenBK7231N_App OpenBK7231N_App 1.0.2 clean
-./build_app.sh apps/OpenBK7231N_App OpenBK7231N_App 1.0.2
+./build_app.sh apps/OpenBK7231N_App OpenBK7231N_App 1.0.3 clean
+./build_app.sh apps/OpenBK7231N_App OpenBK7231N_App 1.0.3
+cp -f apps/OpenBK7231N_App/output/1.0.3/OpenBK7231N_App_1.0.3.rbl /var/www/OpenBK7231N_App_1.0.3.rbl
 
